2864b077cc943f45e1f93260ae0feed7d00fff8b
Alex Antonov <alexantonov@AlexAntonov.local>
2018-03-02 15:54:38 +0200

    ziprepack improved

--- ziprepack.sh
+++ ziprepack.sh
@@ -1,25 +1,35 @@
 #/bin/bash
 #Author: Alex Antonov
 #The program extracts every regular zip archive from the source directory,
-#packs the data to a new password-encrypted RAR archive and then packs the archive again to a regular ZIP archive
+#packs the data to a new password-protected RAR archive and then packs the archive again to a regular ZIP archive
 
-SOURCE=/var/www/chemax/downloads/others
-ENCRYPTED=/var/www/chemax/downloads/othersencrypted
-TARGET=/var/www/chemax/downloads/othersprocessed
-TEMP=/var/www/chemax/downloads/otherstemp
+SOURCE=
+TARGET=
+TEMP=
 COUNTER=0
 PASSWORD=qwertyqwerty
 
-/bin/echo `date +%F" "%T`" -----------------------ziprepack started-------------------------" >> /var/log/zipencrypt.log
+clear_temp () {
+        /bin/rm -rf $TEMP/* >/dev/null 2>&1
+        /bin/rm -rf $TEMP/.* >/dev/null 2>&1
+}
+
+/bin/echo `date +%F" "%T`" -----------------------ziprepack started-------------------------" >> /var/log/ziprepack.log
 for FILE in $(ls $SOURCE); do
         [[ "$FILE" == *.zip ]] || continue
         FILERAR=`echo $FILE | sed -e 's/\.zip/\.rar/g'`
-        /usr/bin/nice -n 5 /usr/bin/unzip -q $SOURCE/$FILE -d $TEMP >>/var/log/zipencrypt.log 2>&1
-        /usr/bin/nice -n 5 /usr/bin/rar a -ep1 -inul -hp$PASSWORD $ENCRYPTED/$FILERAR $TEMP/* >>/var/log/zipencrypt.log 2>&1
-        /usr/bin/nice -n 5 /usr/bin/zip -jq $TARGET/$FILE $ENCRYPTED/$FILERAR >>/var/log/zipencrypt.log 2>&1
-        /bin/rm -rf $TEMP/* >/dev/null 2>&1
-        /bin/rm -rf $TEMP/.* >/dev/null 2>&1
-        /bin/rm -f $ENCRYPTED/$FILERAR >/dev/null 2>&1
+        /usr/bin/nice -n 5 /usr/bin/unzip -q $SOURCE/$FILE -d $TEMP >/dev/null 2>&1 || {
+                echo "$SOURE/$FILE has issues with extraction. Skipping..." >>/var/log/ziprepack.log
+                clear_temp
+                continue
+        }
+        /usr/bin/nice -n 5 /usr/bin/rar a -ep1 -inul -hp$PASSWORD $TEMP/$FILERAR $TEMP/* >/dev/null 2>&1 || {
+                echo "$TEMP/$FILERAR has not been archived properly. Skipping..." >>/var/log/ziprepack.log
+                clear_temp
+                continue
+        }
+        /usr/bin/nice -n 5 /usr/bin/zip -jq $TARGET/$FILE $TEMP/$FILERAR >/dev/null 2>&1 || echo "$TARGET/$FILE has not been archived properly" >>/var/log/ziprepack.log
+        clear_temp
         ((++COUNTER))
 done
-/bin/echo "Number of processed files: $COUNTER" | tee -a /var/log/zipencrypt.log
+/bin/echo "Number of processed files: $COUNTER--------------------------------------------" | tee -a /var/log/ziprepack.log
