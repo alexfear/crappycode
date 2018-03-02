64aa8e3c9ba686deb97b5bf4ebb0e03ac808a2a8
Alex Antonov <alexantonov@AlexAntonov.local>
2018-03-02 11:34:43 +0200

    ziprepack improved

--- ziprepack.sh
+++ ziprepack.sh
@@ -7,16 +7,21 @@ SOURCE=/var/www/chemax/downloads/others
 ENCRYPTED=/var/www/chemax/downloads/othersencrypted
 TARGET=/var/www/chemax/downloads/othersprocessed
 TEMP=/var/www/chemax/downloads/otherstemp
-PASSWORD=qwertyqwerty #password for the archives
 COUNTER=0
+PASSWORD=qwertyqwerty
+
+/bin/echo `date +%F" "%T`" -----------------------ziprepack started-------------------------"
 for FILE in $(ls $SOURCE); do
-        /bin/echo $FILE | grep ".zip" >/dev/null 2>&1 || echo "$FILE is not a ZIP archive" >>/var/log/zypencrypt.log && continue
+        [[ "$FILE" == *.zip ]] || {
+                echo "$FILE is not a ZIP archive" >>/var/log/zipencrypt.log
+                continue
+        }
         FILERAR=`echo $FILE | sed -e 's/\.zip/\.rar/g'`
-        /usr/bin/nice -n 5 /usr/bin/unzip -q $SOURCE/$FILE -d $TEMP || echo "$SOURCE/$FILE failed to unpack" >>/var/log/zypencrypt.log
-        /usr/bin/nice -n 5 /usr/bin/rar a -ep -inul -hp$PASSWORD $ENCRYPTED/$FILERAR $TEMP/* || echo "$ENCRYPTED/$FILERAR failed to create" >>/var/log/zypencrypt.log
-        /usr/bin/nice -n 5 /usr/bin/zip -jq $TARGET/$FILE $ENCRYPTED/$FILERAR || echo "$TARGET/$FILE failed to create" >>/var/log/zypencrypt.log
+        /usr/bin/nice -n 5 /usr/bin/unzip -q $SOURCE/$FILE -d $TEMP >>/var/log/zipencrypt.log 2>&1
+        /usr/bin/nice -n 5 /usr/bin/rar a -ep -inul -hp$PASSWORD $ENCRYPTED/$FILERAR $TEMP/* >>/var/log/zipencrypt.log 2>&1
+        /usr/bin/nice -n 5 /usr/bin/zip -jq $TARGET/$FILE $ENCRYPTED/$FILERAR >>/var/log/zipencrypt.log 2>&1
         /bin/rm -rf $TEMP/*
         /bin/rm -f $ENCRYPTED/$FILERAR
-        ((++COUNTER));
+        ((++COUNTER))
 done
-/bin/echo "Number of processed files: $COUNTER" | tee /var/log/zypencrypt.log
+/bin/echo "Number of processed files: $COUNTER" | tee -a /var/log/zipencrypt.log
