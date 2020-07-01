e54c9478f3b1fbd0082ad9f0047d59b786396c09
Alex Antonov <alexantonov@Alexs-Mac.local>
2020-07-01 18:47:12 +0300

    improvements in vApp querying logic

--- vcd-cleanup.sh
+++ vcd-cleanup.sh
@@ -45,11 +45,28 @@ fi
 
 shift $((OPTIND-1))
 
+# Getting the latest API version for the vCD instance
 API_VERSION=$($CURL -s -i -k -H "Accept:application/*+xml" -X GET https://$VCD_HOST/api/versions | grep -A1 'deprecated="false"' | egrep -o '<Version>[0-9.]+' | cut -d'>' -f2 | tail -n 1)
 echo "Using API version: $API_VERSION"
 
+# Getting a session token
 echo "Logging in..."
 TOKEN=$($CURL -s -i -k -H "Accept:application/*+xml;version=$API_VERSION" -u $VCD_USER@system:$VCD_PASSWORD -X POST https://$VCD_HOST/api/sessions | egrep -o "x-vcloud-authorization\:\ [a-z0-9]+" | cut -d' ' -f2)
 
-echo "vApps that will not be deleted:"
-$CURL -s -i -k -H "Accept:application/*+xml;version=$API_VERSION" -H "x-vcloud-authorization:$TOKEN" -X GET "https://$VCD_HOST/api/query?type=adminVApp&fields=name&filter=name==$MARKER*" | grep -i AdminVAppRecord | egrep -o "name=\"[a-zA-Z0-9!_+-:,;=]+\""| cut -d'"' -f2
+# Querying vApps that will not be removed
+VAPPS_TO_KEEP=$($CURL -s -i -k -H "Accept:application/*+xml;version=$API_VERSION" -H "x-vcloud-authorization:$TOKEN" -X GET "https://$VCD_HOST/api/query?type=adminVApp&fields=name&filter=name==$MARKER*" | grep -i AdminVAppRecord | egrep -o "name=\"[a-zA-Z0-9!_+-:,;=/]+\"" | cut -d'"' -f2)
+echo "The following vApps will not be removed"
+echo $VAPPS_TO_KEEP
+
+# Building a filter expression based on vApp names,
+# it will make sure vApps that must not be deleted,
+# will be excluded from the "vApps to remove" query
+for VAPP_TO_KEEP in $VAPPS_TO_KEEP; do
+  FILTER+="name!=$VAPP_TO_KEEP;"
+done
+FILTER=$(echo $FILTER | sed 's/;$//g')
+
+# Querying vApps to be removed
+VAPPS_TO_REMOVE=$($CURL -s -i -k -H "Accept:application/*+xml;version=$API_VERSION" -H "x-vcloud-authorization:$TOKEN" -X GET "https://$VCD_HOST/api/query?type=adminVApp&fields=name&filterEncoded=true&filter=($FILTER)" | grep -i AdminVAppRecord | egrep -o "href=\"[a-zA-Z0-9!_+-:,;=/]+\"" | cut -d'"' -f2)
+echo "HREFs of the vApps to be removed:"
+echo $VAPPS_TO_REMOVE | tr ' ' '\n'
