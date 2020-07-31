011c3cb48965e2445e7342eee29234b803f17a30
Alex Antonov <alexantonov@Alexs-Mac.local>
2020-07-31 12:20:56 +0300

    Added handler for Org VDC's

--- vcd-cleanup.sh
+++ vcd-cleanup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+## Author: Alex Antonov
 ## This program removes
 ## - vApps
 ## - vApp Templates
-## - Medias(ISO's)
+## - Medias (ISO's)
+## - Catalogs
 ## - Org Networks
 ## - Edge Gateways
-## - Catalogs
 ## - Org VDCs
 ## - Organizations
 
@@ -18,7 +19,7 @@ usage() {
 
 logout() {
   echo "Logging out from the vCD..."
-  $CURL -s -i -k -H "Accept:application/*+xml;version=$3" -H "x-vcloud-authorization:$2" -X DELETE "https://$1/api/session" >/dev/null
+  $CURL -s -i -k -H "Accept:application/*+xml;version=$API_VERSION" -H "x-vcloud-authorization:$TOKEN" -X DELETE "https://$VCD_HOST/api/session" >/dev/null
 }
 
 vapp_deployed() {
@@ -26,29 +27,61 @@ vapp_deployed() {
 }
 
 undeploy_vapp() {
-  UNDEPLOY_TASK=$($CURL -s -i -k -H "Accept:application/*+xml;version=$API_VERSION" -H "x-vcloud-authorization:$TOKEN" -H "Content-Type:application/vnd.vmware.vcloud.undeployVAppParams+xml" -X POST -d '<UndeployVAppParams xmlns="http://www.vmware.com/vcloud/v1.5"><UndeployPowerAction>powerOff</UndeployPowerAction></UndeployVAppParams>' "$1/action/undeploy" | grep -i "<Task" | egrep -o "href=\"[a-zA-Z0-9!_+-:,;=/]+\"" | cut -d'"' -f2)
-  if [ "$UNDEPLOY_TASK" ]; then
-    task_progress $UNDEPLOY_TASK "Undeploying $1... "
+  UNDEPLOY_TASK=$($CURL -s -i -k -H "Accept:application/*+xml;version=$API_VERSION" -H "x-vcloud-authorization:$TOKEN" -H "Content-Type:application/vnd.vmware.vcloud.undeployVAppParams+xml" -X POST -d '<UndeployVAppParams xmlns="http://www.vmware.com/vcloud/v1.5"><UndeployPowerAction>powerOff</UndeployPowerAction></UndeployVAppParams>' "$1/action/undeploy")
+  UNDEPLOY_TASK_HREF=$(echo $UNDEPLOY_TASK | grep -i "<Task" | egrep -o "href=\"[a-zA-Z0-9_+-:,;=/]+\"" | cut -d'"' -f2)
+  UNDEPLOY_TASK_ERROR_MESSAGE=$(echo $UNDEPLOY_TASK | egrep -o "message=\"\[\ [a-zA-Z0-9-]+\ \]\ [a-zA-Z0-9!_+-:,;=/ ]+\"" | cut -d'"' -f2)
+  if [ "$UNDEPLOY_TASK_HREF" ]; then
+    task_progress $UNDEPLOY_TASK_HREF "Undeploying $1... "
+  else
+    echo "Undeploying $1 failed: $UNDEPLOY_TASK_ERROR_MESSAGE"
   fi
 }
 
 delete_entity() {
-  DELETE_TASK=$($CURL -s -i -k -H "Accept:application/*+xml;version=$API_VERSION" -H "x-vcloud-authorization:$TOKEN" -X DELETE "$1" | grep -i "<Task" | egrep -o "href=\"[a-zA-Z0-9!_+-:,;=/]+\"" | cut -d'"' -f2)
-  if [ "$DELETE_TASK" ]; then
-    task_progress $DELETE_TASK "Deleting $1... "
+  DELETE_TASK=$($CURL -s -i -k -H "Accept:application/*+xml;version=$API_VERSION" -H "x-vcloud-authorization:$TOKEN" -X DELETE "$1")
+  DELETE_TASK_HREF=$(echo $DELETE_TASK | grep -i "<Task" | egrep -o "href=\"[a-zA-Z0-9_+-:,;=/]+\"" | cut -d'"' -f2 | tr " " "\n" | head -1)
+  DELETE_TASK_ERROR_MESSAGE=$(echo $DELETE_TASK | egrep -o "message=\"\[\ [a-zA-Z0-9-]+\ \]\ [a-zA-Z0-9!_+-:,;=()&/ ]+\"" | cut -d'"' -f2)
+  if [ "$DELETE_TASK_HREF" ]; then
+    task_progress $DELETE_TASK_HREF "Deleting $1... "
+  else
+    echo "Deleting $1... failed: $DELETE_TASK_ERROR_MESSAGE"
   fi
 }
 
 task_progress() {
   while true; do
-    STATUS=$($CURL -s -i -k -H "Accept:application/*+xml;version=$API_VERSION" -H "x-vcloud-authorization:$TOKEN" -X GET "$1" | egrep -o "status=\"[a-z]+\"" | cut -d'"' -f2 | head -1)
-    printf "$2 $STATUS"\\r
-    if [ "$STATUS" == "success" ] || [ "$STATUS" == "error" ] || [ "$STATUS" == "aborted" ]; then
+    TASK=$($CURL -s -i -k -H "Accept:application/*+xml;version=$API_VERSION" -H "x-vcloud-authorization:$TOKEN" -X GET "$1")
+    TASK_STATUS=$(echo $TASK | egrep -o "status=\"[a-z]+\"" | cut -d'"' -f2 | head -1)
+    TASK_ERROR_MESSAGE=$(echo $TASK | egrep -o "message=\"\[\ [a-zA-Z0-9-]+\ \]\ [a-zA-Z0-9!_+-:,;=()&/ ]+\"" | cut -d'"' -f2)
+    printf "$2 $TASK_STATUS"\\r
+    if [ "$TASK_STATUS" == "success" ]|| [ "$TASK_STATUS" == "aborted" ]; then
       echo
+      break
+    fi
+    if [ "$TASK_STATUS" == "error" ]; then
+      echo "$2 $TASK_STATUS: $TASK_ERROR_MESSAGE"
       break
     fi
     sleep 2
   done
+}
+
+query_filter() {
+  FILTER=''
+  for ENTITY in $@; do
+    FILTER+="name!=$ENTITY;"
+  done
+  echo $FILTER | sed 's/;$//g'
+}
+
+vdc_enabled() {
+  $CURL -s -i -k -H "Accept:application/*+xml;version=$API_VERSION" -H "x-vcloud-authorization:$TOKEN" -X GET "$1" | egrep -o "rel=\"disable\""
+}
+
+disable_vdc() {
+  printf "Disabling $1... "\\r
+  TASK=$($CURL -s -i -k -H "Accept:application/*+xml;version=$API_VERSION" -H "x-vcloud-authorization:$TOKEN" -X POST "$1/action/disable" | egrep -o "204")
+  echo "Disabling $1... ok"
 }
 
 # Parsing arguments
@@ -99,55 +132,102 @@ fi
 echo "The entities which names start with \"$MARKER\" will NOT be deleted"
 
 # Querying vApps to keep
-VAPPS_TO_KEEP=$($CURL -s -i -k -H "Accept:application/*+xml;version=$API_VERSION" -H "x-vcloud-authorization:$TOKEN" -X GET "https://$VCD_HOST/api/query?type=adminVApp&fields=name&filter=name==$MARKER*" | grep -i AdminVAppRecord | egrep -o "name=\"[a-zA-Z0-9!_+-:,;=/]+\"" | cut -d'"' -f2)
+VAPPS_TO_KEEP=$($CURL -s -i -k -H "Accept:application/*+xml;version=$API_VERSION" -H "x-vcloud-authorization:$TOKEN" -X GET "https://$VCD_HOST/api/query?type=adminVApp&fields=name&filter=name==$MARKER*" | grep -i AdminVAppRecord | egrep -o "name=\"[a-zA-Z0-9!_+-:,;=/ ]+\"" | cut -d'"' -f2)
 
 # Building a filter to prevent the vApps from being deleted
 FILTER=''
 if [ "$VAPPS_TO_KEEP" ]; then
-  for VAPP_TO_KEEP in $VAPPS_TO_KEEP; do
-    FILTER+="name!=$VAPP_TO_KEEP;"
-  done
-  FILTER=$(echo $FILTER | sed 's/;$//g')
+  FILTER=$(query_filter $VAPPS_TO_KEEP)
 fi  
 
 # Querying vApps to remove
 echo "+++ The following vApps will be deleted:"
 $CURL -s -i -k -H "Accept:application/*+xml;version=$API_VERSION" -H "x-vcloud-authorization:$TOKEN" -X GET "https://$VCD_HOST/api/query?type=adminVApp&fields=name&filterEncoded=true&filter=$FILTER" | grep -i AdminVAppRecord | egrep -o "name=\"[a-zA-Z0-9!_+-:,;=/ ]+\"" | cut -d'"' -f2 | sed 's/^.*$/\ \ \ \ &/g'
-VAPPS_TO_REMOVE=$($CURL -s -i -k -H "Accept:application/*+xml;version=$API_VERSION" -H "x-vcloud-authorization:$TOKEN" -X GET "https://$VCD_HOST/api/query?type=adminVApp&fields=name&filterEncoded=true&filter=$FILTER" | grep -i AdminVAppRecord | egrep -o "href=\"[a-zA-Z0-9!_+-:,;=/]+\"" | cut -d'"' -f2)
+VAPPS_TO_REMOVE=$($CURL -s -i -k -H "Accept:application/*+xml;version=$API_VERSION" -H "x-vcloud-authorization:$TOKEN" -X GET "https://$VCD_HOST/api/query?type=adminVApp&fields=name&filterEncoded=true&filter=$FILTER" | grep -i AdminVAppRecord | egrep -o "href=\"[a-zA-Z0-9_+-:,;=/]+\"" | cut -d'"' -f2)
 
 # Querying vApps Templates to keep
-VAPP_TEMPLATES_TO_KEEP=$($CURL -s -i -k -H "Accept:application/*+xml;version=$API_VERSION" -H "x-vcloud-authorization:$TOKEN" -X GET "https://$VCD_HOST/api/query?type=adminVAppTemplate&fields=name&filter=name==$MARKER*" | grep -i AdminVAppTemplateRecord | egrep -o "name=\"[a-zA-Z0-9!_+-:,;=/]+\"" | cut -d'"' -f2)
+VAPP_TEMPLATES_TO_KEEP=$($CURL -s -i -k -H "Accept:application/*+xml;version=$API_VERSION" -H "x-vcloud-authorization:$TOKEN" -X GET "https://$VCD_HOST/api/query?type=adminVAppTemplate&fields=name&filter=name==$MARKER*" | grep -i AdminVAppTemplateRecord | egrep -o "name=\"[a-zA-Z0-9!_+-:,;=/ ]+\"" | cut -d'"' -f2)
 
 # Building a filter to prevent the vApp Templates from being deleted
 FILTER=''
 if [ "$VAPP_TEMPLATES_TO_KEEP" ]; then
-  for VAPP_TEMPLATE_TO_KEEP in $VAPP_TEMPLATES_TO_KEEP; do
-    FILTER+="name!=$VAPP_TEMPLATE_TO_KEEP;"
-  done
-  FILTER=$(echo $FILTER | sed 's/;$//g')
+  FILTER=$(query_filter $VAPP_TEMPLATES_TO_KEEP)
 fi 
 
 # Querying vApp Templates to remove
 echo "+++ The following vApp Templates will be deleted:"
 $CURL -s -i -k -H "Accept:application/*+xml;version=$API_VERSION" -H "x-vcloud-authorization:$TOKEN" -X GET "https://$VCD_HOST/api/query?type=adminVAppTemplate&fields=name&filterEncoded=true&filter=$FILTER" | grep -i AdminVAppTemplateRecord | egrep -o "name=\"[a-zA-Z0-9!_+-:,;=/ ]+\"" | cut -d'"' -f2 | sed 's/^.*$/\ \ \ \ &/g'
-VAPP_TEMPLATES_TO_REMOVE=$($CURL -s -i -k -H "Accept:application/*+xml;version=$API_VERSION" -H "x-vcloud-authorization:$TOKEN" -X GET "https://$VCD_HOST/api/query?type=adminVAppTemplate&fields=name&filterEncoded=true&filter=$FILTER" | grep -i AdminVAppTemplateRecord | egrep -o "href=\"[a-zA-Z0-9!_+-:,;=/]+\"" | cut -d'"' -f2)
+VAPP_TEMPLATES_TO_REMOVE=$($CURL -s -i -k -H "Accept:application/*+xml;version=$API_VERSION" -H "x-vcloud-authorization:$TOKEN" -X GET "https://$VCD_HOST/api/query?type=adminVAppTemplate&fields=name&filterEncoded=true&filter=$FILTER" | grep -i AdminVAppTemplateRecord | egrep -o "href=\"[a-zA-Z0-9_+-:,;=/]+\"" | cut -d'"' -f2)
 
-# Querying Medias to keep
-MEDIAS_TO_KEEP=$($CURL -s -i -k -H "Accept:application/*+xml;version=$API_VERSION" -H "x-vcloud-authorization:$TOKEN" -X GET "https://$VCD_HOST/api/query?type=adminMedia&fields=name&filter=name==$MARKER*" | grep -i AdminMediaRecord | egrep -o "name=\"[a-zA-Z0-9!_+-:,;=/]+\"" | cut -d'"' -f2)
+# Querying Media (ISO's) to keep
+MEDIAS_TO_KEEP=$($CURL -s -i -k -H "Accept:application/*+xml;version=$API_VERSION" -H "x-vcloud-authorization:$TOKEN" -X GET "https://$VCD_HOST/api/query?type=adminMedia&fields=name&filter=name==$MARKER*" | grep -i AdminMediaRecord | egrep -o "name=\"[a-zA-Z0-9!_+-:,;=/ ]+\"" | cut -d'"' -f2)
 
 # Building a filter to prevent the Medias from being deleted
 FILTER=''
 if [ "$MEDIAS_TO_KEEP" ]; then
-  for MEDIA_TO_KEEP in $MEDIAS_TO_KEEP; do
-    FILTER+="name!=$MEDIA_TO_KEEP;"
-  done
-  FILTER=$(echo $FILTER | sed 's/;$//g')
+  FILTER=$(query_filter $MEDIAS_TO_KEEP)
 fi
 
-# Querying Medias to remove
-echo "+++ The following Media(ISOs) will be deleted:"
+# Querying Media (ISO's) to remove
+echo "+++ The following Media (ISOs) will be deleted:"
 $CURL -s -i -k -H "Accept:application/*+xml;version=$API_VERSION" -H "x-vcloud-authorization:$TOKEN" -X GET "https://$VCD_HOST/api/query?type=adminMedia&fields=name&filterEncoded=true&filter=$FILTER" | grep -i AdminMediaRecord | egrep -o "name=\"[a-zA-Z0-9!_+-:,;=/ ]+\"" | cut -d'"' -f2 | sed 's/^.*$/\ \ \ \ &/g'
-MEDIAS_TO_REMOVE=$($CURL -s -i -k -H "Accept:application/*+xml;version=$API_VERSION" -H "x-vcloud-authorization:$TOKEN" -X GET "https://$VCD_HOST/api/query?type=adminMedia&fields=name&filterEncoded=true&filter=$FILTER" | grep -i AdminMediaRecord | egrep -o "href=\"[a-zA-Z0-9!_+-:,;=/]+\"" | cut -d'"' -f2)
+MEDIAS_TO_REMOVE=$($CURL -s -i -k -H "Accept:application/*+xml;version=$API_VERSION" -H "x-vcloud-authorization:$TOKEN" -X GET "https://$VCD_HOST/api/query?type=adminMedia&fields=name&filterEncoded=true&filter=$FILTER" | grep -i AdminMediaRecord | egrep -o "href=\"[a-zA-Z0-9_+-:,;=/]+\"" | cut -d'"' -f2)
+
+# Querying Catalogs to keep
+CATALOGS_TO_KEEP=$($CURL -s -i -k -H "Accept:application/*+xml;version=$API_VERSION" -H "x-vcloud-authorization:$TOKEN" -X GET "https://$VCD_HOST/api/query?type=adminCatalog&fields=name&filter=name==$MARKER*" | grep -i AdminCatalogRecord | egrep -o "name=\"[a-zA-Z0-9!_+-:,;=/ ]+\"" | cut -d'"' -f2)
+
+# Building a filter to prevent the Catalogs from being deleted
+FILTER=''
+if [ "$CATALOGS_TO_KEEP" ]; then
+  FILTER=$(query_filter $CATALOGS_TO_KEEP)
+fi
+
+# Querying Catalogs to remove
+echo "+++ The following Catalogs will be deleted:"
+$CURL -s -i -k -H "Accept:application/*+xml;version=$API_VERSION" -H "x-vcloud-authorization:$TOKEN" -X GET "https://$VCD_HOST/api/query?type=adminCatalog&fields=name&filterEncoded=true&filter=$FILTER" | grep -i AdminCatalogRecord | egrep -o "name=\"[a-zA-Z0-9!_+-:,;=/ ]+\"" | cut -d'"' -f2 | sed 's/^.*$/\ \ \ \ &/g'
+CATALOGS_TO_REMOVE=$($CURL -s -i -k -H "Accept:application/*+xml;version=$API_VERSION" -H "x-vcloud-authorization:$TOKEN" -X GET "https://$VCD_HOST/api/query?type=adminCatalog&fields=name&filterEncoded=true&filter=$FILTER" | grep -i AdminCatalogRecord | egrep -o "href=\"[a-zA-Z0-9_+-:,;=/]+\"" | cut -d'"' -f2 | sed 's:catalog:admin\/catalog:g')
+
+# Querying Org Networks to keep
+ORG_NETWORKS_TO_KEEP=$($CURL -s -i -k -H "Accept:application/*+xml;version=$API_VERSION" -H "x-vcloud-authorization:$TOKEN" -X GET "https://$VCD_HOST/api/query?type=orgVdcNetwork&fields=name&filter=name==$MARKER*" | grep -i OrgVdcNetworkRecord | egrep -o "name=\"[a-zA-Z0-9!_+-:,;=/ ]+\"" | cut -d'"' -f2)
+
+# Building a filter to prevent the Org Networks from being deleted
+FILTER=''
+if [ "$ORG_NETWORKS_TO_KEEP" ]; then
+  FILTER=$(query_filter $ORG_NETWORKS_TO_KEEP)
+fi
+
+# Querying Org Networks to remove
+echo "+++ The following Org Networks will be deleted:"
+$CURL -s -i -k -H "Accept:application/*+xml;version=$API_VERSION" -H "x-vcloud-authorization:$TOKEN" -X GET "https://$VCD_HOST/api/query?type=orgVdcNetwork&fields=name&filterEncoded=true&filter=$FILTER" | grep -i OrgVdcNetworkRecord | egrep -o "name=\"[a-zA-Z0-9!_+-:,;=/ ]+\"" | cut -d'"' -f2 | sed 's/^.*$/\ \ \ \ &/g'
+ORG_NETWORKS_TO_REMOVE=$($CURL -s -i -k -H "Accept:application/*+xml;version=$API_VERSION" -H "x-vcloud-authorization:$TOKEN" -X GET "https://$VCD_HOST/api/query?type=orgVdcNetwork&fields=name&filterEncoded=true&filter=$FILTER" | grep -i OrgVdcNetworkRecord | egrep -o "href=\"[a-zA-Z0-9_+-:,;=/]+\"" | cut -d'"' -f2)
+
+# Querying Edge Gateways to keep
+GATEWAYS_TO_KEEP=$($CURL -s -i -k -H "Accept:application/*+xml;version=$API_VERSION" -H "x-vcloud-authorization:$TOKEN" -X GET "https://$VCD_HOST/api/query?type=edgeGateway&fields=name&filter=name==$MARKER*" | grep -i EdgeGatewayRecord | egrep -o "name=\"[a-zA-Z0-9!_+-:,;=/ ]+\"" | cut -d'"' -f2)
+
+# Building a filter to prevent the Edge Gateways from being deleted
+FILTER=''
+if [ "$GATEWAYS_TO_KEEP" ]; then
+  FILTER=$(query_filter $GATEWAYS_TO_KEEP)
+fi
+
+# Querying Edge Gateways to remove
+echo "+++ The following Edge Gateways will be deleted:"
+$CURL -s -i -k -H "Accept:application/*+xml;version=$API_VERSION" -H "x-vcloud-authorization:$TOKEN" -X GET "https://$VCD_HOST/api/query?type=edgeGateway&fields=name&filterEncoded=true&filter=$FILTER" | grep -i EdgeGatewayRecord | egrep -o "name=\"[a-zA-Z0-9!_+-:,;=/ ]+\"" | cut -d'"' -f2 | sed 's/^.*$/\ \ \ \ &/g'
+GATEWAYS_TO_REMOVE=$($CURL -s -i -k -H "Accept:application/*+xml;version=$API_VERSION" -H "x-vcloud-authorization:$TOKEN" -X GET "https://$VCD_HOST/api/query?type=edgeGateway&fields=name&filterEncoded=true&filter=$FILTER" | grep -i EdgeGatewayRecord | egrep -o "href=\"[a-zA-Z0-9_+-:,;=/]+\"" | cut -d'"' -f2)
+
+# Querying Org VDC's to keep
+VDCS_TO_KEEP=$($CURL -s -i -k -H "Accept:application/*+xml;version=$API_VERSION" -H "x-vcloud-authorization:$TOKEN" -X GET "https://$VCD_HOST/api/query?type=adminOrgVdc&fields=name&filter=name==$MARKER*" | grep -i AdminVdcRecord | egrep -o "name=\"[a-zA-Z0-9!_+-:,;=/ ]+\"" | cut -d'"' -f2)
+
+# Building a filter to prevent the Org VDC's from being deleted
+FILTER=''
+if [ "$VDCS_TO_KEEP" ]; then
+  FILTER=$(query_filter $VDCS_TO_KEEP)
+fi
+
+# Querying Org VDC's to remove
+echo "+++ The following Org VDC's will be deleted:"
+$CURL -s -i -k -H "Accept:application/*+xml;version=$API_VERSION" -H "x-vcloud-authorization:$TOKEN" -X GET "https://$VCD_HOST/api/query?type=adminOrgVdc&fields=name&filterEncoded=true&filter=$FILTER" | grep -i AdminVdcRecord | egrep -o "name=\"[a-zA-Z0-9!_+-:,;=/ ]+\"" | cut -d'"' -f2 | sed 's/^.*$/\ \ \ \ &/g'
+VDCS_TO_REMOVE=$($CURL -s -i -k -H "Accept:application/*+xml;version=$API_VERSION" -H "x-vcloud-authorization:$TOKEN" -X GET "https://$VCD_HOST/api/query?type=adminOrgVdc&fields=name&filterEncoded=true&filter=$FILTER" | grep -i AdminVdcRecord | egrep -o "href=\"[a-zA-Z0-9_+-:,;=/]+\"" | cut -d'"' -f2)
 
 # Warning user about consequences and asking to confirm the deletion of the entities
 while true; do
@@ -155,7 +235,7 @@ while true; do
   read -n 1 -p "" yn
   case $yn in
     [Yy]* ) break;;
-    [Nn]* ) echo; logout $VCD_HOST $TOKEN $API_VERSION; exit 0;;
+    [Nn]* ) echo; logout; exit 0;;
     * ) echo "Correct answers are \"y\" and \"n\"";;
   esac
 done
@@ -179,11 +259,42 @@ if [ "$VAPP_TEMPLATES_TO_REMOVE" ]; then
   done
 fi
 
-# Removing Media (ISOs)
+# Removing Media (ISO's)
 if [ "$MEDIAS_TO_REMOVE" ]; then 
   for MEDIA in $MEDIAS_TO_REMOVE; do
     delete_entity $MEDIA
   done
 fi
 
-logout $VCD_HOST $TOKEN $API_VERSION
+# Removing Catalogs
+if [ "$CATALOGS_TO_REMOVE" ]; then 
+  for CATALOG in $CATALOGS_TO_REMOVE; do
+    delete_entity $CATALOG
+  done
+fi
+
+# Removing Org Networks
+if [ "$ORG_NETWORKS_TO_REMOVE" ]; then 
+  for ORG_NETWORK in $ORG_NETWORKS_TO_REMOVE; do
+    delete_entity $ORG_NETWORK
+  done
+fi
+
+# Removing Edge Gateways
+if [ "$GATEWAYS_TO_REMOVE" ]; then 
+  for GATEWAY in $GATEWAYS_TO_REMOVE; do
+    delete_entity $GATEWAY
+  done
+fi
+
+# Removing Org VDC's
+if [ "$VDCS_TO_REMOVE" ]; then 
+  for VDC in $VDCS_TO_REMOVE; do
+    if [ $(vdc_enabled $VDC) ]; then
+      disable_vdc $VDC
+    fi
+    delete_entity $VDC
+  done
+fi
+
+logout
